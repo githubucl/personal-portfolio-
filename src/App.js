212933df1aefc7e43f68f2c527b7f34668288d49
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-06-13 20:23:18 +1200

    fixed pixel ratio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense, useRef } from "react"
 import { Canvas } from "@react-three/fiber"
 import { Environment, Loader } from "@react-three/drei"
+import { isMobile } from 'react-device-detect';
 import Model from "./Model"
 import Overlay from "./Overlay"
 
@@ -15,7 +16,8 @@ export default function App() {
   return (
     <>
       <Canvas
-        pixelRatio={Math.min(3, window.devicePixelRatio)}
+
+        pixelRatio={isMobile ? Math.min(3, window.devicePixelRatio) : window.devicePixelRatio}
         shadows
         onCreated={(state) => state.events.connect(overlay.current)}>
         <ambientLight intensity={0.4} />
