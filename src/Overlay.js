eaf9cba89ce7db28ad65c9d680983ab4c9922960
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-06-26 23:57:59 +1200

    vh

--- src/Overlay.js
+++ src/Overlay.js
@@ -24,10 +24,10 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => {
         <div className="dot">
           <h1>Hey There!</h1>
           <br /><br />
-       My name is <span>Chris Chen</span>. I like Basketball and Apple, but more importantly, I build things for the web
-       <br /><br />
-       Here are a few technologies I've been working with recently:
-       <br /><br />
+          My name is <span>Chris Chen</span>. I like Basketball and Apple, but more importantly, I build things for the web
+          <br /><br />
+          Here are a few technologies I've been working with recently:
+          <br /><br />
           <ul>
             <li>JavaScript (ES6+)</li>
             <li>React + Redux</li>
@@ -64,41 +64,41 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => {
         <div className="dot">
           <h1>Doctor</h1>
           <br /><br />
-       Being a boy in an asian household, becoming a doctor seemed like one of the few career paths that doesn't lead to parents' "disappointment"
-       <br /><br />
-
-       I worked hard and got really good grades in my IB exams and got into med school at <span>University of Melbourne</span>
+          Being a boy in an asian household, becoming a doctor seemed like one of the few career paths that doesn't lead to parents' "disappointment"
           <br /><br />
-       However, it did not take too many lab rats to make me realised that being a doctor might not be an option for me due to my hemophobia
-      </div>
+
+          I worked hard and got really good grades in my IB exams and got into med school at <span>University of Melbourne</span>
+          <br /><br />
+          However, it did not take too many lab rats to make me realised that being a doctor might not be an option for me due to my hemophobia
+        </div>
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
           <h1>Biochemical Engineering</h1>
           <br /><br />
-        While I was hesitating, I got an offer from the other side of the globe -- <span>University College London (UCL) </span>studying Biochemical Engineering
-        <br /><br />
+          While I was hesitating, I got an offer from the other side of the globe -- <span>University College London (UCL) </span>studying Biochemical Engineering
+          <br /><br />
 
-        It did not take me long to make the decision because I did not want to waste the opportunity to see the other side of the world and most importantly, the name sounded much less 'bloody'
-      </div>
+          It did not take me long to make the decision because I did not want to waste the opportunity to see the other side of the world and most importantly, the name sounded much less 'bloody'
+        </div>
       </div>
       <div style={{ height: "250vh" }}>
         <div className="dot gold">
           <h1>Private Equity</h1>
           <br /><br />
-    Most of my London hall mates come from Finance background. Under the influence, I started to be interested in investing. In summer 2019, I got an internship opportunity at a private equity firm as an <span>Analyst</span>
+          Most of my London hall mates come from Finance background. Under the influence, I started to be interested in investing. In summer 2019, I got an internship opportunity at a private equity firm as an <span>Analyst</span>
         </div>
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
           <h1>MBA</h1>
           <br /><br />
-        Upon graduation, I wanted to further my study in business side of the story. Combining with my technical Know-How from my undergraduate study, I will be an ideal candidate for pharmaceutical investment/consulting companies
-        <br /><br />
-I leveraged my undergraduate First Class Honor degree into <span>London School of Economics and Political Science (LSE) & Yale University MBA program</span>
+          Upon graduation, I wanted to further my study in business side of the story. Combining with my technical Know-How from my undergraduate study, I will be an ideal candidate for pharmaceutical investment/consulting companies
+          <br /><br />
+          I leveraged my undergraduate First Class Honor degree into <span>London School of Economics and Political Science (LSE) & Yale University MBA program</span>
         </div>
       </div>
-      <div style={{ height: "250vh" }}>
+      <div style={{ height: "100vh" }}>
         <div className="dot ">
           <h1 className="why">
             BUT WHY
@@ -106,21 +106,21 @@ I leveraged my undergraduate First Class Honor degree into <span>London School o
         </div>
       </div>
 
-      <div style={{ height: "250vh" }}>
+      <div style={{ height: "100vh" }}>
         <div className="dot ">
           <h1 className="why">
             DO ALL OF THESE
           </h1>
         </div>
       </div>
-      <div style={{ height: "250vh" }}>
+      <div style={{ height: "100vh" }}>
         <div className="dot ">
           <h1 className="why">
             HAVE TO DO WITH THE
           </h1>
         </div>
       </div>
-      <div style={{ height: "9vh" }}>
+      <div style={{ height: "459vh" }}>
         <div className="dot ">
           <h1 className="why">
 
