318c9bd232cc4222af2b413ec57f69291dbdfeb5
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-07-05 15:24:13 +1200

    fixed decimal places

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
 import React, { Suspense, useRef } from "react"
 import { Canvas } from "@react-three/fiber"
 import { Environment, Loader } from "@react-three/drei"
-import { isMobile } from 'react-device-detect';
+import { isMobile } from "react-device-detect"
 import Model from "./Model"
 import Overlay from "./Overlay"
-
 
 export default function App() {
   const overlay = useRef()
   const caption = useRef()
   const scroll = useRef(0)
 
-
-
   return (
     <>
-      <Canvas
-
-        dpr={[1, 2]}
-        shadows
-        onCreated={(state) => state.events.connect(overlay.current)}>
+      <Canvas dpr={[1, 2]} shadows onCreated={(state) => state.events.connect(overlay.current)}>
         <ambientLight intensity={0.4} />
         <Suspense fallback={null}>
-          <Model scroll={scroll}
-            raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}
-          />
+          <Model scroll={scroll} raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }} />
           <Environment preset="city" />
         </Suspense>
       </Canvas>
       <Loader
-
         dataInterpolation={(p) => {
-          return ` ${isMobile ? 'Better experience on PC' : `Loading ${p.toFixed(2) * 100}%`}`
+          return ` ${isMobile ? "Better experience on PC" : `Loading ${(p * 100).toFixed(2)}%`}`
         }}
-
       />
       <Overlay ref={overlay} caption={caption} scroll={scroll} />
     </>
